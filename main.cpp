0408915039c0d9f23d6a55c0d772cb13e578b408
rootanvir <tanvirahmed131015@gmail.com>
2026-09-17 21:13:00 +0600

    socket bound successful

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<winsock2.h>
+using namespace std;
+
+#pragma comment(lib,"ws2_32.lib")
+
+using namespace std;
+
+int main(){
+    WSADATA  wsaData;
+    int result = WSAStartup(MAKEWORD(2,2), &wsaData);
+
+    if(result != 0){
+        cout << "WSAStartup failed" <<'\n';
+        return 1;
+    }
+    cout << "winsock initailed" <<'\n';
+    SOCKET sock = socket(AF_INET,SOCK_STREAM,0);
+    if(sock == INVALID_SOCKET){
+        cout << "socket connection failed" << '\n';
+        cout << "Error: "<< WSAGetLastError() << '\n';
+        return 1;
+    }
+    cout << "socket conncetion sucessful!"<<'\n';
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8000);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    cout << "connection configured" << '\n';
+    result = bind(
+        sock,
+        (sockaddr*)&serverAddress,
+        sizeof(serverAddress)
+    );
+    if(result == SOCKET_ERROR){
+        cout << "bind failed" << '\n';
+        cout << "Error: "<< WSAGetLastError() << '\n';
+    }
+    cout << "Socket bound successfully !" << '\n';
+    closesocket(sock);
+    WSACleanup();
+
+    return 0;
+}
